902e407bba4b5a1eef6dba4b7ed6f0416e788a16
Jessica Daye <jddaye@hotmail.com>
2021-09-15 20:22:09 -0400

    Updated with morgan and errors
    Updated to include morgan and an error handler

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+    morgan = require('morgan');
 const app = express();
+app.use(morgan('common'));
 
 let topTenMovies = [
     {
@@ -49,7 +51,15 @@ app.get('/movies', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the first ten movies I could think of');
+    let responseText = 'Welcome to the first ten movies I could think of';
+    res.send(responseText);
+});
+
+app.use(express.static('documentation'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something somewhere broke...')
 });
 
 app.listen(8080, () => {
