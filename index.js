5fa5f7338691b727ca5d4b93a2413a8fdc927ffb
Jessica Daye <jddaye@hotmail.com>
2021-09-21 19:45:17 -0400

    Testing delete express route

--- index.js
+++ index.js
@@ -166,75 +166,16 @@ app.put('/users/:name/:favorites', (req, res) => {
 
 //Removes a movie from the user's favorites
 
-// app.delete('/users/:name/:favorites', (req, res) => {
-//     let user = users.find((user) => { return user.name === req.params.name });
+app.delete('/users/:name/:favorites', (req, res) => {
+    let user = users.find((user) => { return user.name === req.params.name });
 
-//     if (user) {
-//         users = users.filter((obj) => { return obj.favorites !== req.params.favorites });
-//             res.status(201).send(req.params.favorites + ' was deleted.');
-//     }
-// });
+    if (user) {
+        users = users.filter((obj) => { return obj.favorites !== req.params.favorites });
+            res.status(201).send(req.params.favorites + ' was deleted.');
+    }
+});
 
-
-
-// const express = require('express');
-//     morgan = require('morgan');
-// const app = express();
-// app.use(morgan('common'));
-
-// let topTenMovies = [
-//     {
-//         name: 'Harry Potter Series',
-//         year: '2001 - 2011'
-//     },
-//     {
-//         name: 'Troy',
-//         year: '2004'
-//     },
-//     {
-//         name: 'Aladdin',
-//         year: '1992'
-//     },
-//     {
-//         name: 'Beauty and the Beast',
-//         year: '1991'
-//     },
-//     {
-//         name: 'The Little Mermaid',
-//         year: '1989'
-//     },
-//     {
-//         name: 'Whiplash',
-//         year: '2014'
-//     },
-//     {
-//         name: 'Tomb Raider',
-//         year: '2018'
-//     },
-//     {
-//         name: 'Les Miserables',
-//         year: '2012'
-//     },
-//     {
-//         name: 'Silver Linings Playbook',
-//         year: '2012'
-//     },
-//     {
-//         name: 'House at the End of the Street',
-//         year: '2012'
-//     },
-// ];
-
-// app.get('/movies', (req, res) => {
-//     res.json(topTenMovies);
-// });
-
-// app.get('/', (req, res) => {
-//     let responseText = 'Welcome to the first ten movies I could think of';
-//     res.send(responseText);
-// });
-
-// app.use(express.static('documentation'));
+app.use(express.static('documentation'));
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
