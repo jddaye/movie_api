212f765bd21ded7712e6670b8ac86a0116279d0b
Jessica Daye <jddaye@hotmail.com>
2021-10-27 14:28:19 -0400

    Mongoose
    Switched formatting to use mongoose

--- index.js
+++ index.js
@@ -5,197 +5,348 @@ const express = require('express'),
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-let movies = [
-    {
-        movie: 'Silver Linings Playbook',
-        actor: 'Jennifer Lawrence',
-        director: 'David O. Russell',
-        genre: 'Drama'
-    },
+const mongoose = require('mongoose');
+const Models = require('./models.js'),
 
-    {
-        movie: 'About Time',
-        actor: 'Rachel McAdams',
-        director: 'Richard Curtis',
-        genre: 'Fantasy'
-    },
+const Movies = Models.Movie;
+const Users = Models.User;
 
-    {
-        movie: 'Me Before You',
-        actor: 'Emilia Clarke',
-        director: 'Thea Sharrock',
-        genre: 'Romance'
-    },
+mongoose.connect('mongodb://localhost:27017/collectionMovies', {useNewUrlParser: true, useUnifiedTopology: true});
 
-    {
-        movie: 'Joy',
-        actor: 'Jennifer Lawrence',
-        director: 'David O. Russell',
-        genre: 'Biography'
-    },
+// let movies = [
+//     {
+//         movie: 'Silver Linings Playbook',
+//         actor: 'Jennifer Lawrence',
+//         director: 'David O. Russell',
+//         genre: 'Drama'
+//     },
 
-    {
-        movie: 'American Hustle',
-        actor: 'Jennifer Lawrence',
-        director: 'David O. Russell',
-        genre: 'Crime'
-    },
+//     {
+//         movie: 'About Time',
+//         actor: 'Rachel McAdams',
+//         director: 'Richard Curtis',
+//         genre: 'Fantasy'
+//     },
 
-    {
-        movie: 'The Edge of Seventeen',
-        actor: 'Hailee Steinfeld',
-        director: 'Kelly Fremon Craig',
-        genre: 'Comedy'
-    },
+//     {
+//         movie: 'Me Before You',
+//         actor: 'Emilia Clarke',
+//         director: 'Thea Sharrock',
+//         genre: 'Romance'
+//     },
 
-    {
-        movie: 'Someone Great',
-        actor: 'Gina Rodriguez',
-        director: 'Jennifer Kaytin Robinson',
-        genre:  'Comedy'
-    },
-];
+//     {
+//         movie: 'Joy',
+//         actor: 'Jennifer Lawrence',
+//         director: 'David O. Russell',
+//         genre: 'Biography'
+//     },
 
-let users = [
-    {
-        id: 1,
-        name: 'Jessica Drake',
-        favorites: {
-            movie: 'Silver Linings Playbook',
-            movie: 'Someone Great'
-        }
-    },
+//     {
+//         movie: 'American Hustle',
+//         actor: 'Jennifer Lawrence',
+//         director: 'David O. Russell',
+//         genre: 'Crime'
+//     },
 
-    {
-        id: 2,
-        name: 'Ben Cohen',
-        favorites: {
-            movie: 'The Hunger Games',
-            movie: 'House at the End of the Street'
-        }
-    },
+//     {
+//         movie: 'The Edge of Seventeen',
+//         actor: 'Hailee Steinfeld',
+//         director: 'Kelly Fremon Craig',
+//         genre: 'Comedy'
+//     },
 
-    {
-        id: 3,
-        name: 'Lisa Downing',
-        favorites: {
-            movie: 'The Edge of Seventeen',
-            movie: 'Me Before You'
-        }
-    },
-]
+//     {
+//         movie: 'Someone Great',
+//         actor: 'Gina Rodriguez',
+//         director: 'Jennifer Kaytin Robinson',
+//         genre:  'Comedy'
+//     },
+
+//     {
+//         movie: 'Crazy Rich Asians',
+//         actor: 'Constance Wu',
+//         director: 'Jon M. Chu',
+//         genre:  'Drama'
+//     },
+
+//     {
+//         movie: 'Holidate',
+//         actor: 'Emma Roberts',
+//         director: 'John Whitesell',
+//         genre:  'Comedy'
+//     },
+
+//     {
+//         movie: 'Last Christmas',
+//         actor: 'Emilia Clarke',
+//         director: 'Paul Feig',
+//         genre:  'Romance'
+//     },
+// ];
+
+// let users = [
+//     {
+//         id: 1,
+//         name: 'Jessica Drake',
+//         favorites: {
+//             movie: 'Silver Linings Playbook',
+//             movie: 'Someone Great'
+//         }
+//     },
+
+//     {
+//         id: 2,
+//         name: 'Ben Cohen',
+//         favorites: {
+//             movie: 'The Hunger Games',
+//             movie: 'House at the End of the Street'
+//         }
+//     },
+
+//     {
+//         id: 3,
+//         name: 'Lisa Downing',
+//         favorites: {
+//             movie: 'The Edge of Seventeen',
+//             movie: 'Me Before You'
+//         }
+//     },
+// ]
 
 //Gets the list of data about ALL movies
 
 app.get('/movies', (req, res) => {
-    res.json(movies);
+    Movies.find()
+        .then((movies) => {
+            res.status(201).json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
-//Gets the data about a single movie, by name
+//Gets the data about a single movie, by title
 
-app.get('/movies/:movie', (req, res) => {
-    res.json(movies.find((movies) => 
-        { return movies.movie === req.params.movie }));
+app.get('/movies/:Movie', (req, res) => {
+    Movies.findOne({Title: req.params.Title})
+        .then((movies) => {
+            res.json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets the list of data about ALL actors
 
 app.get('/actors', (req, res) => {
-    res.send('All actors works');
+    Actors.find()
+        .then((actors) => {
+            res.status(201).json(actors);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets the data about a single actor, by name
 
-app.get('/actors/:name', (req, res) => {
-    res.send('Single actor works');
+app.get('/actors/:Name', (req, res) => {
+    Actors.findOne({Name: req.params.Name})
+        .then((actors) => {
+            res.json(actors);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets the list of data about ALL directors
 
 app.get('/directors', (req, res) => {
-    res.send('All directors works');
+    Directors.find()
+        .then((directors) => {
+            res.status(201).json(directors);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets the data about a single director, by name
 
-app.get('/directors/:name', (req, res) => {
-    res.send('Single director works');
+app.get('/directors/:Name', (req, res) => {
+    Directors.findOne({Name: req.params.Username})
+        .then((directors) => {
+            res.json(directors);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets the list of data about ALL genres
 
 app.get('/genres', (req, res) => {
-    res.send('All genres works');
+    Genres.find()
+        .then((genres) => {
+            res.status(201).json(genres);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets the data about a single genre, by name
 
-app.get('/genres/:name', (req, res) => {
-    res.send('Single genre works');
+app.get('/genres/:Name', (req, res) => {
+    Genres.findOne({Name: req.params.Name})
+        .then((genres) => {
+            res.json(genres);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets the list of data about ALL users
 
 app.get('/users', (req, res) => {
-    res.send('All users works');
+    Users.find()
+        .then((users) => {
+            res.status(201).json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Gets the data about a single user, by name
 
-app.get('/users/:name', (req, res) => {
-    res.send('Single user works');
+app.get('/users/:Username', (req, res) => {
+    Users.findOne({Username: req.params.Username})
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Adds data for a new user to our list of users
 
 app.post('/users', (req, res) => {
-    let newUser = req.body;
-
-    if (!newUser.name) {
-        const message = 'Missing name in request body';
-        res.status(400).send(message);
-    } else {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).send(newUser);
-    }
+    Users.findOne({Username: req.body.Username})
+        .then((user) => {
+            if(user) {
+                return res.status(400).send(req.body.Username + ' already exists');
+            } else {
+                Users
+                    .create({
+                        Username: req.body.Username,
+                        Password: req.body.Password,
+                        Email: req.body.Email,
+                        Birthday: req.body.Birthday
+                    })
+                    .then((user) => {res.status(201).json(user)})
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).send('Error: ' + error);
+                })
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+        });
 });
 
-//Deletes a user from our list of users
+//Update a user's info, by username
 
-app.delete('users/:name', (req, res) => {
-    let user = users.find((user) => { return user.name === req.params.name });
+app.put('/users/:Username', (req,res) => {
+    Users.findOneAndUpdate({Usernme: req.params.Username}, {
+        $set:
+            {
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+            }
+    },
+    {new: true},
+    (err, updatedUser) => {
+        if(err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        } else {
+            res.json(updatedUser);
+        }
+    });
+});
 
-    if (user) {
-        users = users.filter((obj) => { return obj.name !== req.params.name });
-        res.status(201).send('User ' + req.params.name + ' was deleted.');
-    };
+//Deletes a user from our list of users 
+
+app.delete('/users/:Username', (req, res) => {
+    Users.findOneAndRemove({Username: req.params.Username})
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Adds a movie to the user's favorites
 
-app.put('/users/:name/:favorites', (req, res) => {
-    let user = users.find((user) => { return user.name === req.params.name });
-
-    if (user) {
-        user.favorites[req.params.favorites] = parseInt(req.params.movie);
-        res.status(201).send(req.params.name + " added " + req.params.movie + " to " + req.params.favorites);
-    } else {
-        res.status(404).send(req.params.movie + " was not added to " + req.params.favorites);   
-    }
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({Username: req.params.Username}, {
+        $push: {FavoriteMovies: req.params.MovieID}
+    },
+    {new: true},
+    (err, updatedUser) => {
+        if(err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        } else {
+            res.json(updatedUser);
+        }
+    });
 });
 
 
 //Removes a movie from the user's favorites
 
-app.delete('/users/:name/:favorites', (req, res) => {
-    let user = users.find((user) => { return user.name === req.params.name });
-
-    if (user) {
-        users = users.filter((obj) => { return obj.favorites !== req.params.favorites });
-            res.status(201).send(req.params.favorites + ' was deleted.');
-    }
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({Username: req.params.Username}, {
+        $pull: {FavoriteMovies: req.params.MovieID}
+    },
+    {new: true},
+    (err, updatedUser) => {
+        if(err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        } else {
+            res.json(updatedUser);
+        }
+    });
 });
 
 app.use(express.static('documentation'));
