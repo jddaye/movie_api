aeb1fcf7545174fe149c9f79a64550fbc3f41dd8
Jessica Daye <jddaye@hotmail.com>
2021-11-22 06:33:18 -0500

    test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const {check, validationResult} = require('express-validator');
 
 //Online Host
 mongoose.set('bufferCommands', false);
-mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('process.env.CONNECTION_URI', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
