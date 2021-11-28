d743835c0d58f53d2afca39c563d808190889a9d
Jessica Daye <jddaye@hotmail.com>
2021-11-27 19:51:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get('documentation.html', (req, res) => {
 
 // Gets the list of data about ALL movies
 
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
